8290f5cd5e674d36b29bd2e8b662da9fa679e542
Guillaume Pitois <guillaume.pitois1@gmail.com>
2024-10-13 13:49:47 +0200

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Utiliser le dossier 'public' pour les fichiers statiques
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route principale qui renvoie le fichier HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Démarrer le serveur
+app.listen(port, () => {
+  console.log(`Serveur démarré sur http://localhost:${port}`);
+});
