59346b13cac3e8bb78d9ab4449dbee9e39d2644a
Guillaume Pitois <guillaume.pitois1@gmail.com>
2024-11-03 17:25:04 +0100

    accueil

--- public/app.js
+++ public/app.js
@@ -1,1 +1,27 @@
 console.log('hello world')
+
+const burger = document.querySelector('#burger')
+const nav = document.querySelector('#nav')
+
+
+
+burger.addEventListener("click",()=>{
+    let el = burger.textContent
+    menu(el)
+})
+
+function menu(arg){
+    if(arg === 'menu_open'){
+
+        burger.textContent="tab_close_right"
+        
+        nav.style.display="flex"
+
+    }else if(arg === 'tab_close_right'){
+        burger.textContent="menu_open"
+        
+        nav.style.display="none"
+    }else{
+        console.log('err');
+    }
+}
