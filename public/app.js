3255e80f95a2117c97657a6f592e47a14ab87adf
Guillaume Pitois <guillaume.pitois1@gmail.com>
2024-11-06 23:12:34 +0100

    modif bug

--- public/app.js
+++ public/app.js
@@ -2,6 +2,7 @@ console.log('hello world')
 
 const burger = document.querySelector('#burger')
 const nav = document.querySelector('#nav')
+const a = document.querySelectorAll('.menu')
 
 
 
@@ -13,15 +14,46 @@ burger.addEventListener("click",()=>{
 function menu(arg){
     if(arg === 'menu_open'){
 
-        burger.textContent="tab_close_right"
+        burger.textContent="tab_close_right" 
+        for (let i = 0; i < a.length; i++) {
+            const el = a[i];
+            el.style.transform=`translateX(0px)`
+        }
         
-        nav.style.display="flex"
 
     }else if(arg === 'tab_close_right'){
         burger.textContent="menu_open"
-        
-        nav.style.display="none"
+        let x = 200
+        for (let i = 0; i < a.length; i++) {
+            const el = a[i];
+            x+=20
+            el.style.transform=`translateX(${x}px)`
+            el.classList.remove('shadow')
+        }
+
     }else{
-        console.log('err');
+        arg === 'menu_open'
     }
 }
+
+
+async function loadImages() {
+    try {
+        const response = await fetch('/api/images');
+        if (!response.ok) throw new Error('Erreur lors de la récupération des images');
+        const images = await response.json();
+
+        // Utilisez le tableau d'images pour configurer le carrousel
+        console.log(images); // Vous pouvez l'utiliser dans votre fonction carrousel
+        // Exemple : afficher chaque image dans un élément img
+        const carouselContainer = document.getElementById('carousel');
+        // images.forEach(imagePath => {
+        //     const imgElement = document.createElement('img');
+        //     imgElement.src = imagePath;
+        //     carouselContainer.appendChild(imgElement);
+        // });
+    } catch (error) {
+        console.error('Erreur:', error);
+    }
+}
+
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs');
 const app = express();
 const port = 9999;
 
@@ -10,6 +10,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Route principale qui renvoie le fichier HTML
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+
+
+function getImagePaths() {
+  const imagesDir = path.join(__dirname, 'Public/img');
+  return fs.readdirSync(imagesDir)
+      .filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file))
+      .map(file => `/Public/img/${file}`);
+}
+
+
+app.get('/api/images', (req, res) => {
+  const images = getImagePaths();
+  res.json(images);
 });
 
 // Démarrer le serveur
